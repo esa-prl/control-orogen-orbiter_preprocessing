e2865d2315c05863105f71208e22d5bb9de65f74
Dimitris Geromichalos <geromidg@gmail.com>
2018-01-09 11:27:13 +0100

    Cleanup auto-generated code

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -1,54 +1,16 @@
-/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
-
 #include "Task.hpp"
 
-using namespace pcl_io_preprocessing;
+namespace pcl_io_preprocessing {
 
-Task::Task(std::string const& name)
-    : TaskBase(name)
-{
-}
+bool Task::configureHook(void) {
+    if (!TaskBase::configureHook()) return false;
 
-Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
-    : TaskBase(name, engine)
-{
-}
-
-Task::~Task()
-{
-}
-
-
-
-/// The following lines are template definitions for the various state machine
-// hooks defined by Orocos::RTT. See Task.hpp for more detailed
-// documentation about them.
-
-bool Task::configureHook()
-{
-    if (! TaskBase::configureHook())
-        return false;
     return true;
 }
-bool Task::startHook()
-{
-    if (! TaskBase::startHook())
-        return false;
-    return true;
-}
-void Task::updateHook()
-{
+
+void Task::updateHook(void) {
     TaskBase::updateHook();
 }
-void Task::errorHook()
-{
-    TaskBase::errorHook();
-}
-void Task::stopHook()
-{
-    TaskBase::stopHook();
-}
-void Task::cleanupHook()
-{
-    TaskBase::cleanupHook();
-}
+
+}  // namespace pcl_io_preprocessing
+
